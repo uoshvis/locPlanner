37935c7f482d256a11ce2918c3501d385fb7aa8b
uoshvis <uoshvis@gmail.com>
2022-08-05 12:23:11 +0300

    add select and edit on drag and resize

--- src/features/calendar/Calendar.js
+++ src/features/calendar/Calendar.js
@@ -1,47 +1,65 @@
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
-
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { updateEvent } from "./eventsSlice";
 
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
 
 
 function MainCalendar() {
-    const initialEvents = useSelector(state => state.events)
-     
-    const [events, setEvents] = useState(initialEvents)
+    const dispatch = useDispatch()
+    const events = useSelector(state => state.events.items)
 
-    const onEventResize = (data) => {
-        console.log(data)
+    const [currentEvent, setCurrentEvent] = useState({})
 
+    const handleEventResize = (data) => {
+        const { start, end } = data
+        const updatedEvent = {
+            ...data.event,
+            start: start.toString(),
+            end: end.toString()
+        }
+        dispatch(updateEvent(updatedEvent))
     };
 
-    const onEventDrop = (data) => {
-        console.log(data)
-
+    const handleEventDrop = (data) => {
+        const { start, end } = data
+        const updatedEvent = {
+            ...data.event,
+            start: start.toString(),
+            end: end.toString()
+        }
+        dispatch(updateEvent(updatedEvent))
     };
+
+    const handleSelectEvent = (data) => {
+        setCurrentEvent(data)
+    }
 
 
     return (
         <div className="MainCalendar">
             <DnDCalendar
+                style={{ height: "100vh" }}
                 defaultDate={moment().toDate()}
                 defaultView="month"
                 events={events}
                 localizer={localizer}
-                onEventDrop={onEventDrop}
-                onEventResize={onEventResize}
                 resizable
-                style={{ height: "100vh" }}
+                selectable
+                onEventDrop={handleEventDrop}
+                onEventResize={handleEventResize}
+                onSelectEvent={handleSelectEvent}
             />
         </div>
     );
-
 }
+
 
 export default MainCalendar;
