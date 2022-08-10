09936a004c16c56115fa5e2a9af259c6648018ff
uoshvis <uoshvis@gmail.com>
2022-08-10 13:14:05 +0300

    feat: add get by location hadler

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,15 +1,48 @@
 import { rest } from 'msw'
+import moment from 'moment';
 
-const data = []
+const data = [      
+    {
+        id: 10,
+        start: moment(),
+        end: moment().add(1, "hours"),
+        title: "Some title",
+        location: 'loc1',
+    },
+    {   
+        id: 20,
+        start: moment().add(2, "days"),
+        end: moment().add(3, "days"),
+        title: "Some title2",
+        location: 'loc2',
+    },
+]
 
 export const handlers = [
 
-    rest.get('/events', (req, res, ctx) => {
-        const events = data
+    rest.get('/events/:location', (req, res, ctx) => {
+        const { location } = req.params
+        var events = []
+        const validLocation = ['all', 'loc1', 'loc2'].includes(location)
 
+        if (validLocation) {
+            if(location === 'all') {
+                events = data
+            }
+            else {
+                events = data.filter(item => item.location === location)
+            }   
         return res(
             ctx.status(200),
             ctx.json(events)
-        )
-    })
+            )         
+        }
+        else {
+            return res(
+                ctx.status(404, 'Location not found')
+            )
+        }
+
+    }),
+
 ]
