6c4b9175b39da1302c440d20a355b7953278eb39
uoshvis <uoshvis@gmail.com>
2022-09-08 11:29:38 +0300

    fix: update to work with array

--- src/features/calendar/calendarHelpers.js
+++ src/features/calendar/calendarHelpers.js
@@ -3,9 +3,22 @@ import moment from 'moment';
 // convert str dateTime to Date obj
 export const jsonDateTimeConverter = (data) => {
   //.toISOString causes RBC crash on view change  
-  data.forEach(item => {
-      item.start = moment(item.start).toDate();
-      item.end = moment(item.end).toDate();
-    });
-    return data
+  if (
+    typeof data === 'object' &&
+    !Array.isArray(data) &&
+    data !== null
+  ) {
+      data.start = moment(data.start).toDate();
+      data.end = moment(data.end).toDate();
   }
+  else if (
+    Array.isArray(data) &&
+    data !== null
+  ) {
+    data.forEach(item => {
+        item.start = moment(item.start).toDate();
+        item.end = moment(item.end).toDate();
+      });
+  }
+  return data
+}
