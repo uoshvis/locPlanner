1b1568f6fe59d1e0b81bcc6df36bbadae1ee4732
uoshvis <uoshvis@gmail.com>
2022-09-09 15:07:36 +0300

    feat: add react router and responsive app bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import './App.css';
 import MainCalendar from './features/calendar/Calendar';
 import Notification from './features/notification/Notification';
 import { getNotificationType, isNotificationOpen, getNotificationMsg } from './features/notification/notificationSlice';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 
 function App() {
@@ -12,14 +15,23 @@ function App() {
   const notificationMsg = useSelector(getNotificationMsg)
   const open = useSelector(state => state.calendar.showModal)
 
-
   return (
     <div className="App">
-      {
-        notificationIsOpen && !open && <Notification type={notificationType} message={notificationMsg}/>
-      }
-      <MainCalendar />
-
+        <Navbar />
+            <React.Fragment>
+                {
+                    notificationIsOpen &&
+                    !open && 
+                    <Notification 
+                        type={notificationType}
+                        message={notificationMsg}
+                    />
+                }                
+            </React.Fragment>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="calendar" element={<MainCalendar />} />           
+        </Routes>
     </div>
   );
 }
