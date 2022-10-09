18c2620ae6cbb6a6f6a0cc65be7764a31b0e3e4f
uoshvis <uoshvis@gmail.com>
2022-10-09 17:02:33 +0100

    feat: add account page sample

---
+++ src/components/Account.js
@@ -0,0 +1,12 @@
+function Account() {
+    return (
+      <>
+        <main>
+          <h2>Welcome to the Account page!</h2>
+          <p>You can do this, I believe in you.</p>
+        </main>
+      </>
+    );
+  }
+
+  export default Account
