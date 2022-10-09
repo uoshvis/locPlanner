7a8defbf60b2c0113d005408c495235d3012c73f
uoshvis <uoshvis@gmail.com>
2022-10-09 07:46:14 +0100

    feat: rejected case

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -19,13 +19,20 @@ export const authSlice = createSlice({
             .addCase(login.pending, (state, action) => {
                 state.status = 'loading'
             })
+            .addCase(login.rejected, (state, action) => { // ToDo Add reject case for invalid user
+                state.status = 'failed'
+                state.error = action.error.message
+            })
             .addCase(login.fulfilled, (state, action) => {
                 state.isLoggedIn = true
                 state.status = 'succeeded'
-        
             })
             .addCase(logout.pending, (state, action) => {
                 state.status = 'loading'
+            })
+            .addCase(logout.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
             })
             .addCase(logout.fulfilled, (state, action) => {
                 state.isLoggedIn = false
@@ -42,6 +49,7 @@ export const login = createAsyncThunk('auth/login', async (crediantials) => {
 
 export const logout = createAsyncThunk('auth/logout', async (crediantials) => {
     const response = await client.post('/myApi/logout', crediantials)
+    localStorage.removeItem('token');
     return response.data
 })
 
