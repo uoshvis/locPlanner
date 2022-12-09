98232021f50b5d7b2cd4be47200b2ad051318069
uoshvis <uoshvis@gmail.com>
2022-12-09 16:00:29 +0200

    fix: add status fallback value

--- src/components/BackDropLoader.js
+++ src/components/BackDropLoader.js
@@ -2,8 +2,8 @@ import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 
 
-function BackDropLoader({ apiStatus }) {
-
+function BackDropLoader({ apiStatus = '' }) {
+    
     return (
         <Backdrop
             sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
