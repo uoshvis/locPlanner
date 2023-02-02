0f16163fecc95c76dc00fb03966b0a3b71d2b5cc
uoshvis <uoshvis@gmail.com>
2023-02-02 12:11:01 +0200

    feat: prettier and delete user thunk

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,10 +1,15 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { client } from '../../mocks/client'
 
 const initialState = []
 
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async() => {
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
     const response = await client.get('/myApi/users')
+    return response.data
+})
+
+export const deleteUser = createAsyncThunk('users/deleteUser', async (id) => {
+    const response = await client.delete(`/myApi/users/${id}`, id)
     return response.data
 })
 
@@ -17,23 +22,20 @@ const usersSlice = createSlice({
             .addCase(fetchUsers.fulfilled, (state, action) => {
                 return action.payload
             })
-    }
+            .addCase(deleteUser.fulfilled, (state, action) => {})
+    },
 })
 
 export default usersSlice.reducer
 
-export const getUserById = (state, userId) =>
-  {
-    return state.users.find(user => user.id === Number(userId))
-  }
+export const getUserById = (state, userId) => {
+    return state.users.find((user) => user.id === Number(userId))
+}
 
-export const getUserColors = (state) =>
-    {
-        var userColors = state.users.reduce(
-            (obj, item) => {
-                obj[item.id] = item.userColor; 
-                return obj
-            }, {}
-        )
-        return userColors
-    }
+export const getUserColors = (state) => {
+    var userColors = state.users.reduce((obj, item) => {
+        obj[item.id] = item.userColor
+        return obj
+    }, {})
+    return userColors
+}
