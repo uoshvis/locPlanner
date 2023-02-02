887908e482f62c9f67bc364e9159f701d10c6db9
uoshvis <uoshvis@gmail.com>
2023-02-02 12:11:51 +0200

    fix: prettier and delete notifications

--- src/features/notification/notificationSlice.js
+++ src/features/notification/notificationSlice.js
@@ -1,21 +1,28 @@
-import { createEntityAdapter, createSlice, isAnyOf } from '@reduxjs/toolkit';
-import { fetchEventsByLocation, addEventData, updateEventData, deleteEvent } from '../events/eventsSlice';
-import { fetchUsers } from '../users/usersSlice';
-import { login, logout, fetchUserDetails, fetchUserInfo, updateUser } from '../auth/authSlice';
-
+import { createEntityAdapter, createSlice, isAnyOf } from '@reduxjs/toolkit'
+import {
+    fetchEventsByLocation,
+    addEventData,
+    updateEventData,
+    deleteEvent,
+} from '../events/eventsSlice'
+import { fetchUsers, deleteUser } from '../users/usersSlice'
+import {
+    login,
+    logout,
+    fetchUserDetails,
+    fetchUserInfo,
+    updateUser,
+} from '../auth/authSlice'
 
 const notificationAdapter = createEntityAdapter()
 
 // Available types: error, warning, info, success
-const initialState = notificationAdapter.getInitialState(
-    {
-       message: null,
-       type: null,
-       open: false,
-       apiStatus: 'idle' // 'idle' | 'loading' only 2 <<-- | 'succeeded' | 'failed', 
-   }    
-)
-
+const initialState = notificationAdapter.getInitialState({
+    message: null,
+    type: null,
+    open: false,
+    apiStatus: 'idle', // 'idle' | 'loading' only 2 <<-- | 'succeeded' | 'failed',
+})
 
 export const notificationSlice = createSlice({
     name: 'notification',
@@ -24,97 +31,94 @@ export const notificationSlice = createSlice({
         setNotification: (state, action) => {
             state.message = action.payload.message
             state.type = action.payload.type
-            state.open = true        
+            state.open = true
         },
-        clearNotification: state => {
+        clearNotification: (state) => {
             state.message = null
             state.type = null
             state.open = false
-        }
-      },
-      extraReducers: builder => {
+        },
+    },
+    extraReducers: (builder) => {
         builder
-          .addMatcher(
-            isAnyOf(
-                fetchEventsByLocation.rejected,
-            ),
-            (state, action) => {
-                state.message = action.error.message
-                state.type = 'error'
-                state.open = true
-            }
-          )
-          // match pending, fulfilled and rejected
-          .addMatcher(
-            isAnyOf(
-                login.pending,
-                logout.pending,
-                fetchUsers.pending,
-                fetchUserDetails.pending,
-                fetchUserInfo.pending,
-                fetchEventsByLocation.pending,
-                addEventData.pending,
-                updateEventData.pending,
-                deleteEvent.pending,
-                updateUser.pending,
-
-            ),
-            (state, action) => {
-                state.apiStatus = 'loading'
-                state.message = null
-                state.type = null
-                state.open = false
-            }
-          )
-          .addMatcher(
-            isAnyOf(
-                login.fulfilled,
-                logout.fulfilled,
-                fetchUsers.fulfilled,
-                fetchUserDetails.fulfilled,
-                fetchUserInfo.fulfilled,
-                fetchEventsByLocation.fulfilled,
-                addEventData.fulfilled,
-                updateEventData.fulfilled,
-                deleteEvent.fulfilled,
-                updateUser.fulfilled,
-
-            ),
-            (state, action) => {
-                state.apiStatus = 'idle'
-            }
-          )
-          .addMatcher(
-            isAnyOf(
-                login.rejected,
-                logout.rejected,
-                fetchUsers.rejected,
-                fetchUserDetails.rejected,
-                fetchUserInfo.rejected,
-                fetchEventsByLocation.rejected,
-                addEventData.rejected,
-                updateEventData.rejected,
-                deleteEvent.rejected,
-                updateUser.rejected,
-            ),
-            // set rejected status as 'idle' for now
-            (state, action) => {
-                state.apiStatus = 'idle'
-                state.message = action.error.message
-                state.type = 'error'
-                state.open = true
-            }
-          )
-      },
+            .addMatcher(
+                isAnyOf(fetchEventsByLocation.rejected),
+                (state, action) => {
+                    state.message = action.error.message
+                    state.type = 'error'
+                    state.open = true
+                }
+            )
+            // match pending, fulfilled and rejected
+            .addMatcher(
+                isAnyOf(
+                    login.pending,
+                    logout.pending,
+                    fetchUsers.pending,
+                    fetchUserDetails.pending,
+                    fetchUserInfo.pending,
+                    fetchEventsByLocation.pending,
+                    addEventData.pending,
+                    updateEventData.pending,
+                    deleteEvent.pending,
+                    updateUser.pending,
+                    deleteUser.pending
+                ),
+                (state, action) => {
+                    state.apiStatus = 'loading'
+                    state.message = null
+                    state.type = null
+                    state.open = false
+                }
+            )
+            .addMatcher(
+                isAnyOf(
+                    login.fulfilled,
+                    logout.fulfilled,
+                    fetchUsers.fulfilled,
+                    fetchUserDetails.fulfilled,
+                    fetchUserInfo.fulfilled,
+                    fetchEventsByLocation.fulfilled,
+                    addEventData.fulfilled,
+                    updateEventData.fulfilled,
+                    deleteEvent.fulfilled,
+                    deleteUser.fulfilled,
+                    updateUser.fulfilled
+                ),
+                (state, action) => {
+                    state.apiStatus = 'idle'
+                }
+            )
+            .addMatcher(
+                isAnyOf(
+                    login.rejected,
+                    logout.rejected,
+                    fetchUsers.rejected,
+                    fetchUserDetails.rejected,
+                    fetchUserInfo.rejected,
+                    fetchEventsByLocation.rejected,
+                    addEventData.rejected,
+                    updateEventData.rejected,
+                    deleteEvent.rejected,
+                    deleteUser.rejected,
+                    updateUser.rejected
+                ),
+                // set rejected status as 'idle' for now
+                (state, action) => {
+                    state.apiStatus = 'idle'
+                    state.message = action.error.message
+                    state.type = 'error'
+                    state.open = true
+                }
+            )
+    },
 })
 
-
-export const getNotificationMsg = state => state.notification.message
-export const getNotificationType = state => state.notification.type
-export const isNotificationOpen = state => state.notification.open
-export const getApiStatus = state => state.notification.apiStatus
+export const getNotificationMsg = (state) => state.notification.message
+export const getNotificationType = (state) => state.notification.type
+export const isNotificationOpen = (state) => state.notification.open
+export const getApiStatus = (state) => state.notification.apiStatus
 
 export const { clearNotification, setNotification } = notificationSlice.actions
 
 export default notificationSlice.reducer
-
