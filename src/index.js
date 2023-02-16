51ed59e803f9345c667b3212ba316b829d6602e0
uoshvis <uoshvis@gmail.com>
2023-02-16 08:18:29 +0200

    fix: formar and onUnhandledRequest

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import { QueryParamProvider } from 'use-query-params';
-import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import './index.css'
+import { QueryParamProvider } from 'use-query-params'
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6'
 
-
-const container = document.getElementById('root');
-const root = createRoot(container);
+const container = document.getElementById('root')
+const root = createRoot(container)
 
 if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser')
-  worker.start()
+    const { worker } = require('./mocks/browser')
+    worker.start({
+        onUnhandledRequest: 'warn', // "bypass"/ "warn"/ "error"
+    })
 }
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-        <BrowserRouter>
-            <QueryParamProvider adapter={ReactRouter6Adapter}>
-                <App />
-            </QueryParamProvider>
-        </BrowserRouter>     
-    </Provider>
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <QueryParamProvider adapter={ReactRouter6Adapter}>
+                    <App />
+                </QueryParamProvider>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
