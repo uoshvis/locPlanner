0e645b66998e9f8ed7af3da78ceed1c817317cf6
uoshvis <uoshvis@gmail.com>
2023-02-17 19:24:28 +0200

    feat: add loginPage e2e test

---
+++ cypress/e2e/loginPage.cy.js
@@ -0,0 +1,55 @@
+describe('Login Page', () => {
+    it('loads successfully', () => {
+        // ARRANGE
+        cy.visit('http://localhost:3000')
+
+        // ACT
+
+        // ASSERT
+
+        // Navbar
+        cy.get('.MuiPaper-root')
+            .should('be.visible')
+            .within(() => {
+                cy.get('.MuiTypography-root').should(
+                    'contain.text',
+                    'LocPlanner'
+                )
+                cy.get('a')
+                    .should('be.visible')
+                    .should('contain.text', 'LocPlanner')
+            })
+        // Login Form
+        cy.get('h1').should('contain.text', 'Sign in')
+        cy.get('form').should('be.visible')
+        cy.get('input')
+            .get('[name="userName"]')
+            .should('be.visible')
+            .should('have.focus')
+        cy.get('input').get('[name="password"]').should('be.visible')
+        cy.get('button').should('contain.text', 'Sign In').should('be.enabled')
+    })
+
+    it('SantaClaus logins and welcomes Santa', () => {
+        //ARRANGE
+        const testUser = {
+            userName: 'SantaClaus',
+            password: '123',
+            firstName: 'Santa',
+        }
+
+        // ACT
+        cy.get('input[name="userName"]').type(testUser.userName)
+        cy.get('input#password').type(testUser.password)
+        cy.get('button').should('be.enabled')
+        cy.get('form').submit()
+
+        // ASSERT
+        cy.get('.MuiToolbar-root').within(() => {
+            cy.get('p').should(
+                'contain.text',
+                `Welcome back ${testUser.firstName}`
+            )
+        })
+    })
+})
