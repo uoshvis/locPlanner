1258ac734ae725c4a841a7af0682f5c13f6e0f2b
uoshvis <uoshvis@gmail.com>
2023-02-19 18:22:46 +0200

    fix: add matcher to match actions endsWith

--- src/features/notification/notificationSlice.js
+++ src/features/notification/notificationSlice.js
@@ -1,30 +1,24 @@
-import { createEntityAdapter, createSlice, isAnyOf } from '@reduxjs/toolkit'
-import {
-    fetchEventsByLocation,
-    addEventData,
-    updateEventData,
-    deleteEvent,
-    deleteEvents,
-} from '../events/eventsSlice'
-import {
-    fetchUsers,
-    fetchUser,
-    createUser,
-    updateUser,
-    deleteUser,
-} from '../users/usersSlice'
-import { login, register, logout } from '../auth/authSlice'
-import { fetchMeetings } from '../meetings/meetingsSlice'
-
-const notificationAdapter = createEntityAdapter()
+import { createSlice } from '@reduxjs/toolkit'
 
 // Available types: error, warning, info, success
-const initialState = notificationAdapter.getInitialState({
+const initialState = {
     message: null,
     type: null,
     open: false,
     apiStatus: 'idle', // 'idle' | 'loading' only 2 <<-- | 'succeeded' | 'failed',
-})
+}
+
+function isPendingAction(action) {
+    return action.type.endsWith('/pending')
+}
+
+function isFulfilledAction(action) {
+    return action.type.endsWith('/fulfilled')
+}
+
+function isRejectedAction(action) {
+    return action.type.endsWith('/rejected')
+}
 
 export const notificationSlice = createSlice({
     name: 'notification',
@@ -43,77 +37,21 @@ export const notificationSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder
-            // match pending, fulfilled and rejected
-            .addMatcher(
-                isAnyOf(
-                    register.pending,
-                    login.pending,
-                    logout.pending,
-                    fetchUsers.pending,
-                    fetchUser.pending,
-                    fetchEventsByLocation.pending,
-                    addEventData.pending,
-                    updateEventData.pending,
-                    deleteEvent.pending,
-                    createUser.pending,
-                    updateUser.pending,
-                    deleteUser.pending,
-                    deleteEvents.pending,
-                    fetchMeetings.pending
-                ),
-                (state, action) => {
-                    state.apiStatus = 'loading'
-                    state.message = null
-                    state.type = null
-                    state.open = false
-                }
-            )
-            .addMatcher(
-                isAnyOf(
-                    register.fulfilled,
-                    login.fulfilled,
-                    logout.fulfilled,
-                    fetchUsers.fulfilled,
-                    fetchUser.fulfilled,
-                    fetchEventsByLocation.fulfilled,
-                    addEventData.fulfilled,
-                    updateEventData.fulfilled,
-                    deleteEvent.fulfilled,
-                    createUser.fulfilled,
-                    deleteUser.fulfilled,
-                    updateUser.fulfilled,
-                    deleteEvents.fulfilled,
-                    fetchMeetings.fulfilled
-                ),
-                (state, action) => {
-                    state.apiStatus = 'idle'
-                }
-            )
-            .addMatcher(
-                isAnyOf(
-                    register.rejected,
-                    login.rejected,
-                    logout.rejected,
-                    fetchUsers.rejected,
-                    fetchUser.rejected,
-                    fetchEventsByLocation.rejected,
-                    addEventData.rejected,
-                    updateEventData.rejected,
-                    deleteEvent.rejected,
-                    createUser.rejected,
-                    deleteUser.rejected,
-                    updateUser.rejected,
-                    deleteEvents.rejected,
-                    fetchMeetings.rejected
-                ),
-                // set rejected status as 'idle' for now
-                (state, action) => {
-                    state.apiStatus = 'idle'
-                    state.message = action.error.message
-                    state.type = 'error'
-                    state.open = true
-                }
-            )
+            .addMatcher(isPendingAction, (state, action) => {
+                state.apiStatus = 'loading'
+                state.message = null
+                state.type = null
+                state.open = false
+            })
+            .addMatcher(isFulfilledAction, (state, action) => {
+                state.apiStatus = 'idle'
+            })
+            .addMatcher(isRejectedAction, (state, action) => {
+                state.apiStatus = 'idle'
+                state.message = action.error.message
+                state.type = 'error'
+                state.open = true
+            })
     },
 })
 
