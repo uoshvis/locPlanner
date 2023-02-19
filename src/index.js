5f0d943409f0a734e919dffd0e75bb17319dc9db
uoshvis <uoshvis@gmail.com>
2023-02-19 13:07:48 +0200

    fix: bypass onUnhandledRequest warn

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const root = createRoot(container)
 if (process.env.NODE_ENV === 'development') {
     const { worker } = require('./mocks/browser')
     worker.start({
-        onUnhandledRequest: 'warn', // "bypass"/ "warn"/ "error"
+        onUnhandledRequest: 'bypass', // "bypass"/ "warn"/ "error"
     })
 }
 
