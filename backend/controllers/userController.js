afd997151fd9beaffc9e4b536db7a938253c20c4
uoshvis <uoshvis@gmail.com>
2023-03-09 16:39:56 +0200

    fix: update userController to match form data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,10 +3,10 @@ import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { firstName, email, password } = req.body
+    const data = req.body
 
     // check if email exists in db
-    const userExists = await User.findOne({ email })
+    const userExists = await User.findOne({ userName: data.userName })
 
     if (userExists) {
         res.status(404)
@@ -14,13 +14,13 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     // create new user document in db
-    const user = await User.create({ firstName, email, password })
+    const user = await User.create(data)
 
     if (user) {
         res.status(201).json({
             _id: user._id,
             firstName: user.firstName,
-            email: user.email,
+            userName: user.userName,
         })
     } else {
         res.status(400)
@@ -29,22 +29,23 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
+    const { userName, password } = req.body
 
     // check if user email exists in db
-    const user = await User.findOne({ email })
+    const user = await User.findOne({ userName })
 
     // return user obj if their password matches
     if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
+            userName: user.userName,
             firstName: user.firstName,
-            email: user.email,
+            lastName: user.lastName,
             userToken: generateToken(user._id),
         })
     } else {
         res.status(401)
-        throw new Error('Invalid email or password')
+        throw new Error('Invalid userName or password')
     }
 })
 
@@ -55,8 +56,8 @@ const getUserProfile = asyncHandler(async (req, res) => {
     if (user) {
         res.json({
             id: user._id,
+            userName: user.userName,
             firstName: user.firstName,
-            email: user.email,
         })
     } else {
         res.status(404)
