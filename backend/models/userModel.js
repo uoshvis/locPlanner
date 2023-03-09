e17ab12ab5fde0246546ad8ebc891b98f072dc4d
uoshvis <uoshvis@gmail.com>
2023-03-09 16:40:20 +0200

    fix: update userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,35 +2,55 @@ import bcrypt from 'bcryptjs'
 import { mongoose } from 'mongoose'
 
 const userSchema = mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: false,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: 'user',
+        },
+        isActive: {
+            type: Boolean,
+            default: false,
+        },
+        userColor: {
+            type: String,
+            default: '#f44336',
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 )
 
 // hash user's password with salt before saving document to db
 userSchema.pre('save', async function () {
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 // extend matchPassword function unto userSchema
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password)
+    return await bcrypt.compare(enteredPassword, this.password)
 }
 
 const User = mongoose.model('User', userSchema)
