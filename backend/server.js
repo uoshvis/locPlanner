9a3212fe032a3b96c25c86cc4473f47a98ee22e1
uoshvis <uoshvis@gmail.com>
2023-03-10 16:29:43 +0200

    feat: to allow cors with Authorization bearer

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,11 @@ const app = express()
 
 // CORS on ExpressJS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
     // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
+        'Origin, X-Requested-With, Content-Type, Authorization, Accept'
     )
     next()
 })
