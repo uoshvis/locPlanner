fbafc9105ba7b18285b18a7df38e29b3e83ef3de
uoshvis <uoshvis@gmail.com>
2023-03-11 15:26:44 +0200

    feat: extract RequireLogin and RequireAdmin from App

---
+++ frontend/src/routing/RequireLogin.js
@@ -0,0 +1,14 @@
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
+
+function RequireLogin({ children }) {
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
+
+    let location = useLocation()
+    if (!isLoggedIn) {
+        return <Navigate to={'/login'} state={{ from: location }} replace />
+    }
+    return children ? children : <Outlet /> // to use as wrapping component
+}
+
+export default RequireLogin
