b62474c1e220cdce9b778738e57021a569bcbaa7
uoshvis <uoshvis@gmail.com>
2023-05-06 07:44:16 +0300

    feat: event selectby id and filter by loc cache

--- frontend/src/features/events/eventsSlice.js
+++ frontend/src/features/events/eventsSlice.js
@@ -1,6 +1,28 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSelector, createSlice } from '@reduxjs/toolkit'
 import { client } from '../../mocks/client.js'
 import { dateTimeToDateObj } from './eventsHelpers.js'
+import { eventsApi } from '../../app/services/events/eventsService.js'
+
+export const selectEventsResult = eventsApi.endpoints.getEvents.select()
+
+const emptyEvents = []
+
+export const selectAllEvents = createSelector(
+    selectEventsResult,
+    (eventResult) => eventResult?.data ?? emptyEvents
+)
+
+export const selectEventById = createSelector(
+    selectAllEvents,
+    (state, eventId) => eventId,
+    (events, eventId) => events.find((event) => event.id === eventId)
+)
+
+export const filterEventsByLoc = createSelector(
+    selectAllEvents,
+    (state, eventLoc) => eventLoc,
+    (events, eventLoc) => events.filter((event) => event.location === eventLoc)
+)
 
 const initialState = {
     items: [],
