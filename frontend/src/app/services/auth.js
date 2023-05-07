43f4c482e4fa4bd7abc7fa9feec4a566aacf8f88
uoshvis <uoshvis@gmail.com>
2023-05-07 12:26:57 +0300

    feat: login with rtk api

--- frontend/src/app/services/auth.js
+++ frontend/src/app/services/auth.js
@@ -1,27 +1,21 @@
-// // React-specific entry point to allow generating React hooks
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { api } from './api'
 
-// export const authApi = createApi({
-//     reducerPath: 'authApi',
-//     baseQuery: fetchBaseQuery({
-//         baseUrl: 'http://127.0.0.1:5000/',
-//         prepareHeaders: (headers, { getState }) => {
-//             const token = getState().auth.userToken
-//             if (token) {
-//                 headers.set('authorization', `Bearer ${token}`)
-//                 return headers
-//             }
-//         },
-//     }),
-//     endpoints: (builder) => ({
-//         getUserProfile: builder.query({
-//             query: () => ({
-//                 url: 'api/users/profile',
-//                 method: 'GET',
-//             }),
-//         }),
-//     }),
-// })
-// // export hooks for usage in functional components
-// // auto-generated based on the defined endpoints
-// export const { useGetUserProfileQuery } = authApi
+export const authApi = api.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query(body) {
+                return {
+                    url: 'api/auth/login',
+                    method: 'POST',
+                    body,
+                }
+            },
+        }),
+    }),
+})
+
+export const { useLoginMutation } = authApi
+
+export const {
+    endpoints: { login },
+} = authApi
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,7 +1,5 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
-
-const backendURL = 'http://127.0.0.1:5000'
+import { createSlice } from '@reduxjs/toolkit'
+import { authApi } from '../../app/services/auth'
 
 // initialize userToken from local storage
 const userToken = localStorage.getItem('userToken')
@@ -28,63 +26,85 @@ export const authSlice = createSlice({
         },
     },
     extraReducers(builder) {
-        builder.addCase(login.fulfilled, (state, action) => {
-            state.isLoggedIn = true
-            state.userInfo = action.payload
-            state.userToken = action.payload.userToken
-        })
+        builder
+            // .addCase(login.fulfilled, (state, action) => {
+            //     state.isLoggedIn = true
+            //     state.userInfo = action.payload
+            //     state.userToken = action.payload.userToken
+            // })
+            .addMatcher(
+                authApi.endpoints.login.matchFulfilled,
+                (state, action) => {
+                    state.isLoggedIn = true
+                    state.userInfo = action.payload
+                    state.userToken = action.payload.userToken
+                    localStorage.setItem('userToken', action.payload.userToken)
+                }
+            )
+            .addMatcher(
+                authApi.endpoints.login.matchPending,
+                (state, action) => {
+                    console.log('pending login')
+                }
+            )
+            .addMatcher(
+                authApi.endpoints.login.matchRejected,
+                (state, action) => {
+                    console.log('rejected', action)
+                }
+            )
     },
 })
 
-export const register = createAsyncThunk(
-    'auth/register',
-    async (data, { rejectWithValue }) => {
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }
-            await axios.post(`${backendURL}/api/auth/register`, data, config)
-        } catch (error) {
-            // return custom error message from backend if present
-            if (error.response && error.response.data.message) {
-                return rejectWithValue(error.response.data)
-            } else {
-                return rejectWithValue(error.message)
-            }
-        }
-    }
-)
+// export const register = createAsyncThunk(
+//     'auth/register',
+//     async (data, { rejectWithValue }) => {
+//         try {
+//             const config = {
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//             }
+//             await axios.post(`${backendURL}/api/auth/register`, data, config)
+//         } catch (error) {
+//             // return custom error message from backend if present
+//             if (error.response && error.response.data.message) {
+//                 return rejectWithValue(error.response.data)
+//             } else {
+//                 return rejectWithValue(error.message)
+//             }
+//         }
+//     }
+// )
 
-export const login = createAsyncThunk(
-    'auth/login',
-    async ({ userName, password }, { rejectWithValue }) => {
-        try {
-            // configure header's Content-Type as JSON
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }
-            const { data } = await axios.post(
-                `${backendURL}/api/auth/login`,
-                { userName, password },
-                config
-            )
-            // store user's token in local storage
-            localStorage.setItem('userToken', data.userToken)
-            return data
-        } catch (error) {
-            // return custom error message from API if any
-            if (error.response && error.response.data.message) {
-                return rejectWithValue(error.response.data.message)
-            } else {
-                return rejectWithValue(error.message)
-            }
-        }
-    }
-)
+// export const login = createAsyncThunk(
+//     'auth/login',
+//     async ({ userName, password }, { rejectWithValue }) => {
+//         try {
+//             // configure header's Content-Type as JSON
+//             const config = {
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//             }
+//             const { data } = await axios.post(
+//                 `${backendURL}/api/auth/login`,
+//                 { userName, password },
+//                 config
+//             )
+//             // store user's token in local storage
+//             localStorage.setItem('userToken', data.userToken)
+//             return data
+//         } catch (error) {
+//             // return custom error message from API if any
+//             if (error.response && error.response.data.message) {
+//                 return rejectWithValue(error.response.data.message)
+//             } else {
+//                 return rejectWithValue(error.message)
+//             }
+//         }
+//     }
+// )
 
 export const { logout, setUserInfo } = authSlice.actions
 
