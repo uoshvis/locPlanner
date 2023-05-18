a06bccd00e266a037c915a7cc40fc8948c77139c
uoshvis <uoshvis@gmail.com>
2023-05-18 11:32:58 +0300

    feat: add meetings api rkt query endpoints #68

--- frontend/src/app/services/meetings.js
+++ frontend/src/app/services/meetings.js
@@ -1,17 +1,65 @@
 import { api } from './api'
 
 export const meetingsApi = api.injectEndpoints({
+    tagTypes: ['Meetings'],
     endpoints: (builder) => ({
         getMeetings: builder.query({
-            query: () => ({
+            query: (arg) => ({
                 url: 'api/meetings',
+                params: arg,
+                method: 'GET',
+            }),
+            providesTags: ['Meetings'],
+        }),
+        getMeeting: builder.query({
+            query: (id) => ({
+                url: `api/meetings/${id}`,
                 method: 'GET',
             }),
         }),
-        getMeetingById: builder.query({
-            query: (id) => ({ url: `api/meetings/${id}`, method: 'GET' }),
+        createMeeting: builder.mutation({
+            query: (body) => ({
+                url: `api/meetings`,
+                method: 'POST',
+                body,
+            }),
+            invalidatesTags: ['Meetings'],
+        }),
+        updateMeeting: builder.mutation({
+            query(data) {
+                const { id, ...body } = data
+                return {
+                    url: `api/meetings/${id}`,
+                    method: 'PUT',
+                    body,
+                }
+            },
+            invalidatesTags: ['Meetings'],
+        }),
+        deleteMeeting: builder.mutation({
+            query: (id) => ({
+                url: `api/meetings/${id}/delete`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Meetings'],
+        }),
+        deleteMeetings: builder.mutation({
+            query: (body) => ({
+                url: `api/meetings/delete`,
+                method: 'DELETE',
+                body,
+            }),
+            invalidatesTags: ['Meetings'],
         }),
     }),
 })
-
-export const { useGetMeetingsQuery } = meetingsApi
+// export hooks for usage in functional components
+// auto-generated based on the defined endpoints
+export const {
+    useGetMeetingsQuery,
+    useGetMeetingQuery,
+    useCreateMeetingMutation,
+    useUpdateMeetingMutation,
+    useDeleteMeetingMutation,
+    useDeleteMeetingsMutation,
+} = meetingsApi
