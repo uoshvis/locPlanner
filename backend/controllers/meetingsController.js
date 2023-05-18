14cdcf5878bda0c47d43f53e2d0c826ff44958a3
uoshvis <uoshvis@gmail.com>
2023-05-18 11:23:14 +0300

    feat: add meetings api routes and controllers #68

--- backend/controllers/meetingsController.js
+++ backend/controllers/meetingsController.js
@@ -12,4 +12,55 @@ const getMeetings = asyncHandler(async (req, res) => {
     }
 })
 
-export { getMeetings }
+const getMeeting = asyncHandler(async (req, res) => {
+    const id = req.params.id
+    const meeting = await Meeting.find({ id })
+
+    if (meeting) {
+        res.json(meeting)
+    } else {
+        res.status(404)
+        throw new Error(`Meeting with id ${id} not found`)
+    }
+})
+
+const createMeeting = asyncHandler(async (req, res) => {
+    const data = req.body
+    const id = Date.now()
+
+    const meeting = await Meeting.create({ id, ...data })
+
+    if (meeting) {
+        res.status(201).json({
+            id: meeting.id,
+            title: meeting.title,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid meeting data')
+    }
+})
+
+const updateMeeting = asyncHandler(async (req, res) => {
+    // req.user was set in authMiddleware.js
+    const id = req.params.id
+    const data = req.body
+    const itemToEdit = await Meeting.findOne({ id })
+
+    if (itemToEdit) {
+        const edited = await Meeting.findOneAndUpdate({ id }, data, {
+            new: true,
+        })
+        if (edited) {
+            res.json(edited)
+        } else {
+            res.status(500)
+            throw new Error('Meeting not edited')
+        }
+    } else {
+        res.status(404)
+        throw new Error('Meeting not Found')
+    }
+})
+
+export { getMeetings, getMeeting, createMeeting, updateMeeting }
--- backend/routes/meetingsRoutes.js
+++ backend/routes/meetingsRoutes.js
@@ -5,5 +5,10 @@ import * as meetingsController from '../controllers/meetingsController.js'
 const router = express.Router()
 
 router.route('/').get(protect, meetingsController.getMeetings)
+router.route('/').post(protect, meetingsController.createMeeting)
+router.route('/:id').get(protect, meetingsController.getMeeting)
+router.route('/:id').put(protect, meetingsController.updateMeeting)
+// router.route('/:id/delete').delete(protect, meetingsController.deleteMeeting)
+// router.route('/delete').delete(protect, meetingsController.deleteMeetings)
 
 export default router
