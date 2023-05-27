945407a9c2637de541fddb0eee0a3ab4e5d1c101
uoshvis <uoshvis@gmail.com>
2023-05-27 09:25:57 +0300

    fix: msw for development only

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,11 @@ worker.start({
 })
 // }
 
+if (process.env.NODE_ENV === 'development') {
+    const { worker } = require('./mocks/browser')
+    worker.start()
+}
+
 root.render(
     <React.StrictMode>
         <Provider store={store}>
