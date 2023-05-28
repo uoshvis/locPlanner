89f140d8704712ed0ffd720850abcd3f47119079
uoshvis <uoshvis@gmail.com>
2023-05-28 17:33:40 +0300

    fix: selectTexField after merge conflict

--- frontend/src/features/users/formFields/SelectTextField.js
+++ frontend/src/features/users/formFields/SelectTextField.js
@@ -21,20 +21,6 @@ const userAdminRoles = [
     },
 ]
 
-<<<<<<< HEAD:src/features/users/formFields/SelectTextField.js
-function SelectTextField({
-    control,
-    name,
-    label,
-    id,
-    readOnly,
-    required = false,
-}) {
-    const [options, setOptions] = useState(userAdminRoles)
-||||||| d0ae4e2:src/features/users/formFields/SelectTextField.js
-function SelectTextField({ control, name, label, id, readOnly }) {
-    const [options, setOptions] = useState(userAdminRoles)
-=======
 const rolesAll = userAdminRoles.concat(superAdminRole)
 
 function SelectTextField({
@@ -46,7 +32,6 @@ function SelectTextField({
     required = false,
 }) {
     const [options, setOptions] = useState(rolesAll)
->>>>>>> development:frontend/src/features/users/formFields/SelectTextField.js
 
     const {
         field,
@@ -79,15 +64,9 @@ function SelectTextField({
             label={label}
             error={Boolean(error)}
             helperText={error?.message}
-<<<<<<< HEAD:src/features/users/formFields/SelectTextField.js
-            required={required}
-            fullWidth
-||||||| d0ae4e2:src/features/users/formFields/SelectTextField.js
-=======
             required={required}
             fullWidth
             margin="normal"
->>>>>>> development:frontend/src/features/users/formFields/SelectTextField.js
         >
             {options.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
