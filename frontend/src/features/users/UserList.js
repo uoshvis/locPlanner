16710344d45dc2bec2338d274c175e72e6cd330b
uoshvis <uoshvis@gmail.com>
2023-05-31 17:11:46 +0300

    feat: change table structure and loading data spinner #83

--- frontend/src/features/users/UserList.js
+++ frontend/src/features/users/UserList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
@@ -15,10 +16,12 @@ import Stack from '@mui/material/Stack'
 import UserFormDialog from './UserFormDialog'
 import TextField from '@mui/material/TextField'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+import { CircularIndeterminate } from '../../components/Spinners'
 
 export default function UserList({ users = [], isSuperAdmin }) {
     const navigate = useNavigate()
     const [searchParams, setSearchParams] = useSearchParams()
+    const { isLoading } = useSelector((state) => state.notification)
 
     const searchTerm = searchParams.get('name') || ''
 
@@ -31,6 +34,72 @@ export default function UserList({ users = [], isSuperAdmin }) {
             setSearchParams({})
         }
     }
+
+    const tableHead = (
+        <TableHead>
+            <TableRow>
+                <TableCell align="center">Firstname</TableCell>
+                <TableCell align="left">Lastname</TableCell>
+                <TableCell align="left">UserName</TableCell>
+                <TableCell align="left">Role</TableCell>
+                <TableCell align="left">Active</TableCell>
+                <TableCell align="left"></TableCell>
+            </TableRow>
+        </TableHead>
+    )
+
+    const tableBody = (
+        <TableBody>
+            {users
+                .filter((user) =>
+                    user.firstName
+                        .toLowerCase()
+                        .includes(searchTerm.toLocaleLowerCase())
+                )
+                .map((user) => (
+                    <TableRow
+                        key={user.id}
+                        sx={{
+                            '&:last-child td, &:last-child th': {
+                                border: 0,
+                            },
+                            textDecoration: 'none',
+                        }}
+                        hover={true}
+                    >
+                        <TableCell align="left" component="th" scope="row">
+                            <CardHeader
+                                avatar={
+                                    <Avatar
+                                        sx={{
+                                            color: user.userColor,
+                                            bgcolor: '#fff',
+                                        }}
+                                    />
+                                }
+                                title={user.firstName}
+                            />
+                        </TableCell>
+                        <TableCell align="left">{user.lastName}</TableCell>
+                        <TableCell align="left">{user.userName}</TableCell>
+                        <TableCell align="left">{user.role}</TableCell>
+                        <TableCell align="left">
+                            {user.isActive ? ' Yes' : 'No'}
+                        </TableCell>
+                        <TableCell align="left">
+                            <Button
+                                disabled={!isSuperAdmin}
+                                onClick={() => {
+                                    navigate(`${user.id}/`)
+                                }}
+                            >
+                                Edit
+                            </Button>
+                        </TableCell>
+                    </TableRow>
+                ))}
+        </TableBody>
+    )
 
     return (
         <Box>
@@ -63,78 +132,14 @@ export default function UserList({ users = [], isSuperAdmin }) {
                 {isSuperAdmin && <UserFormDialog />}
             </Stack>
             <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">Firstname</TableCell>
-                            <TableCell align="left">Lastname</TableCell>
-                            <TableCell align="left">UserName</TableCell>
-                            <TableCell align="left">Role</TableCell>
-                            <TableCell align="left">Active</TableCell>
-                            <TableCell align="left"></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {users
-                            .filter((user) =>
-                                user.firstName
-                                    .toLowerCase()
-                                    .includes(searchTerm.toLocaleLowerCase())
-                            )
-                            .map((user) => (
-                                <TableRow
-                                    key={user.id}
-                                    sx={{
-                                        '&:last-child td, &:last-child th': {
-                                            border: 0,
-                                        },
-                                        textDecoration: 'none',
-                                    }}
-                                    hover={true}
-                                >
-                                    <TableCell
-                                        align="left"
-                                        component="th"
-                                        scope="row"
-                                    >
-                                        <CardHeader
-                                            avatar={
-                                                <Avatar
-                                                    sx={{
-                                                        color: user.userColor,
-                                                        bgcolor: '#fff',
-                                                    }}
-                                                />
-                                            }
-                                            title={user.firstName}
-                                        />
-                                    </TableCell>
-                                    <TableCell align="left">
-                                        {user.lastName}
-                                    </TableCell>
-                                    <TableCell align="left">
-                                        {user.userName}
-                                    </TableCell>
-                                    <TableCell align="left">
-                                        {user.role}
-                                    </TableCell>
-                                    <TableCell align="left">
-                                        {user.isActive ? ' Yes' : 'No'}
-                                    </TableCell>
-                                    <TableCell align="left">
-                                        <Button
-                                            disabled={!isSuperAdmin}
-                                            onClick={() => {
-                                                navigate(`${user.id}/`)
-                                            }}
-                                        >
-                                            Edit
-                                        </Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                    </TableBody>
-                </Table>
+                {isLoading ? (
+                    <CircularIndeterminate />
+                ) : (
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        {tableHead}
+                        {tableBody}
+                    </Table>
+                )}
             </TableContainer>
         </Box>
     )
