5a7e192ab2ffd77cb803109f20a5af3f7a188951
uoshvis <uoshvis@gmail.com>
2023-05-31 16:08:07 +0300

    feat: add more spinner types

---
+++ frontend/src/components/Spinners.js
@@ -0,0 +1,45 @@
+import '../styles/spinner.css'
+import Box from '@mui/material/Box'
+import Backdrop from '@mui/material/Backdrop'
+import CircularProgress from '@mui/material/CircularProgress'
+
+const SpinnerBtn = () => {
+    return (
+        <div className="spinner" aria-label="spinner-icon">
+            <div className="spinner-circle"></div>
+        </div>
+    )
+}
+
+const BackdropCircular = ({ open }) => {
+    return (
+        <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={open}
+        >
+            <CircularProgress color="inherit" />
+        </Backdrop>
+    )
+}
+
+const CircularIndeterminate = () => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                minHeight: '80vh',
+            }}
+        >
+            <CircularProgress
+                sx={{
+                    margin: '0 auto',
+                }}
+            />
+        </Box>
+    )
+}
+
+export { CircularIndeterminate, BackdropCircular, SpinnerBtn }
